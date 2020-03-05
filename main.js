bf2e26d587890b0c12469e80d7815d4c6e457628
Pratomo Ardianto <ardnet@gmail.com>
2020-03-05 21:05:35 +0800

    Add main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+import './js/searchbar.js';
+import './js/column.js';
+
+window.addEventListener('load', () => {
+  
+});
+
+document.getElementById('add-column').addEventListener('click', (e) => {
+    addCol();
+});
